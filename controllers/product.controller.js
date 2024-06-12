72d1bf83cd06f615068368e654921428428ea21b
Samson Aderonmu <143148845+samsonHRC@users.noreply.github.com>
2024-06-12 08:37:42 +0100

    chore: Fix error handling in product controller

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -61,12 +61,12 @@ const editProduct = async (req, res) => {
         const { id } = req.params
         const product = await Product.findByIdAndUpdate(id, req.body)
         if (!product) {
-            res.status(404).json({ message: '"Error updating product' });
+            return res.status(404).json({ message: '"Error updating product' });
         }
         const updatedProduct = await Product.findById(id);
-        res.status(200).json({ message: 'Product updated successfully', product: updatedProduct });
+        return res.status(200).json({ message: 'Product updated successfully', product: updatedProduct });
     } catch (error) {
-        res.status(500).json({ message: "Error updating product", error: error.message });
+        return res.status(500).json({ message: "Error updating product", error: error.message });
     }
 }
 
@@ -77,7 +77,7 @@ const deleteProduct = async (req, res) => {
         if (!product) {
             return res.status(404).json({ message: 'Product not found' });
         }
-        return  res.status(200).json({ message: 'Product deleted successfully', product: product });
+        return res.status(200).json({ message: 'Product deleted successfully', product: product });
     } catch (error) {
         return res.status(500).json({ message: "Error deleting product", error: error.message });
     }
