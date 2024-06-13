68e6d4aa589e92a0dd4991dbb6d098bafb944e16
Samson Aderonmu <143148845+samsonHRC@users.noreply.github.com>
2024-06-13 08:31:02 +0100

    feat: Add image field to product model

--- models/product.model.js
+++ models/product.model.js
@@ -16,6 +16,10 @@ const productSchema = new mongoose.Schema({
         type: Number,
         required: [true, 'Product Quantity is required']
     },
+    image: {
+        type: String,
+        required: false,
+    },
 
     Timestamp: {
         type: Date,
